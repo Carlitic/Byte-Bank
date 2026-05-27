88dc806e41cadd6c897955539cadd7e9155832d6
Carlitic <carloscastanosblanco@gmail.com>
2026-05-27 06:12:43 +0200

    feat: add bytebank-backend

---
+++ bytebank-backend/src/main/java/org/bytebank/backend/BytebankBackendApplication.java
@@ -0,0 +1,13 @@
+package org.bytebank.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytebankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytebankBackendApplication.class, args);
+	}
+
+}
